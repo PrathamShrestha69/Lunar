63007e16bce9ebb0a5e6b8d51c16d494d708501f
Pratham943 <phoneixwasfound69@gmail.com>
2026-01-08 14:37:14 +0545

    logo-changed

--- components/CardBg.tsx
+++ components/CardBg.tsx
@@ -10,63 +10,60 @@ const CardBg = ({ curtain }: { curtain: CurtainType }) => {
   const [hover, isHovered] = useState<boolean>(false);
 
   return (
-    <div className="flex flex-col font-[DM_Sans]">
-      <Link href={`/${curtain.id}`} className="block">
-        <div
-          onMouseEnter={() => isHovered(true)}
-          onMouseLeave={() => isHovered(false)}
-          className="bg-[#ffffff] h-[95vw] lg:h-[36vw] w-[70vw] lg:w-[28vw] items-center rounded-2xl lg:px-5 cursor-pointer overflow-hidden relative group"
-        >
-          <div className="justify-between items-center flex p-3 lg:py-5 relative z-10">
-            <div
-              className={`${curtain.tag === "New"
-                ? "bg-blue-600"
-                : curtain.tag === "Popular"
-                  ? "bg-black"
-                  : "bg-red-600"
-                } text-white w-min px-3 py-1 rounded-full text-xs lg:text-sm`}
-            >
-              {curtain.tag}
-            </div>
-            <div className="bg-[#F5F5F5] p-2 rounded-full hover:bg-white transition-colors">
-              <Icon icon="tdesign:heart" />
-            </div>
+    <Link href={`/${curtain.id}`} className="block font-[DM_Sans]">
+      <div
+        onMouseEnter={() => isHovered(true)}
+        onMouseLeave={() => isHovered(false)}
+        className="bg-white h-[60vw] lg:h-[36vw] w-[45vw] lg:w-[28vw] rounded-2xl lg:px-5 cursor-pointer overflow-hidden relative group flex flex-col justify-between"
+      >
+        <div className="justify-between items-center flex p-3 lg:py-5 relative z-10">
+          <div
+            className={`${curtain.tag === "New"
+              ? "bg-blue-600"
+              : curtain.tag === "Popular"
+                ? "bg-black"
+                : "bg-red-600"
+              } text-white w-min px-3 py-1 rounded-full text-xs lg:text-sm`}
+          >
+            {curtain.tag}
           </div>
-          <div className="relative w-full h-3/4 px-3">
-            <img
-              src={hover ? curtain.image.back : curtain.image.side}
-              alt={curtain.name}
-              className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[calc(100%-1.5rem)] h-[80%] object-cover rounded-2xl transition-all duration-300"
-            />
+          <div className="bg-[#F5F5F5] p-2 rounded-full hover:bg-white transition-colors">
+            <Icon icon="tdesign:heart" />
           </div>
-          {hover && (
-            <motion.div
-              initial={{ y: 20, opacity: 0 }}
-              animate={{ y: 0, opacity: 1 }}
-              exit={{ y: 20, opacity: 0 }}
-              transition={{ duration: 0.3, ease: "easeOut" }}
-              className="absolute bottom-5 left-3 right-3 bg-[#F5F5F5] backdrop-blur-sm rounded-xl p-4 "
-            >
-              <p className="text-sm text-gray-700 line-clamp-2">{curtain.desc}</p>
-            </motion.div>
-          )}
         </div>
-
-      </Link>
-      <Link href={`/${curtain.id}`} className="text-black mt-3 block">
-        <h1 className="font-bold text-base lg:text-lg">{curtain.name}</h1>
-        <div className="flex items-center gap-2 mt-1">
-          {curtain.discountedPrice ? (
-            <>
-              <p className="font-semibold text-lg text-red-600">€{curtain.discountedPrice}</p>
-              <p className="text-sm text-gray-500 line-through">€{curtain.price}</p>
-            </>
-          ) : (
-            <p className="font-semibold text-lg">€{curtain.price}</p>
-          )}
+        <div className="relative w-full h-3/4 px-3">
+          <img
+            src={hover ? curtain.image.back : curtain.image.side}
+            alt={curtain.name}
+            className="absolute top-[12vw] left-1/2 -translate-x-1/2 -translate-y-1/2 w-[calc(100%-1.5rem)] h-[80%] object-cover rounded-2xl transition-all duration-300"
+          />
         </div>
-      </Link>
-    </div>
+        {hover && (
+          <motion.div
+            initial={{ y: 20, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: 20, opacity: 0 }}
+            transition={{ duration: 0.3, ease: "easeOut" }}
+            className="absolute bottom-5 left-3 right-3 bg-[#F5F5F5] backdrop-blur-sm rounded-xl p-4"
+          >
+            <p className="text-sm text-gray-700 line-clamp-2">{curtain.desc}</p>
+          </motion.div>
+        )}
+        <div className="flex flex-col gap-1 px-4 pb-10 text-black">
+          <h1 className="font-bold text-base lg:text-lg leading-tight">{curtain.name}</h1>
+          <div className="flex items-center gap-2">
+            {curtain.discountedPrice ? (
+              <>
+                <p className="font-semibold text-lg text-red-600">€{curtain.discountedPrice}</p>
+                <p className="text-sm text-gray-500 line-through">€{curtain.price}</p>
+              </>
+            ) : (
+              <p className="font-semibold text-lg">€{curtain.price}</p>
+            )}
+          </div>
+        </div>
+      </div>
+    </Link>
   );
 };
 
